017275072499ffd5b3a5c55255143b429f3cbce4
Daniel Teixeira <ddtxra@gmail.com>
2017-07-11 14:43:23 +0200

    adding time

--- scripts/generate-insights-report.scala
+++ scripts/generate-insights-report.scala
@@ -2,14 +2,13 @@ import swiss.sib.analytics.server.logs._
 import swiss.sib.analytics.server.logs.utils.LConfigUtils
 import swiss.sib.analytics.server.logs.utils.LogEntryUtils
 
-//Can not initialize counter due to context is not a instance of TaskInputOutputContext
-//Logger.getLogger("parquet").setLevel(Level.OFF);
-
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val config = LConfigUtils.readConfigFile(System.getProperty("config.file"));
 
 val name = scala.io.StdIn.readLine("Resource name in Insights (ex. STRING) : ")
 val year = scala.io.StdIn.readLine("Year (ex. 2016) : ").toInt
+
+val start = System.currentTimeMillis();
 
 val df = sqlContext.read.parquet(config.parquetFile.getPath).filter($"year" === year)
 
@@ -27,16 +26,6 @@ def prt(s: String, fw: java.io.FileWriter = null) = {
 }
 
 val maxResults = 100;
-/*val dimensions = List("server",
-  //"responseInfo.contentPresent", 
-  "responseInfo.charset",
-  //"agentInfo.isBot", "agentInfo.isProgram",
-  //"agentInfo.bot", "agentInfo.program",
-  //"requestInfo.url", "requestInfo.firstLevelPath",
-  //"locationInfo.city", "locationInfo.country",
-  //"entryType.database", "entryType.accession", 
-  "clientInfo.ipAddress");
-*/
 
 val metrics = List(
   ("server_hits", count("*") as "server_hits", 
@@ -100,6 +89,8 @@ metrics.foreach(m => {
     }
   }
 })
+
+println("Finished in " + (System.currentTimeMillis() - start) / (60 * 1000.0) + " min")
 
 //df.groupBy($"month", $"year").agg(count("*") as "hits").orderBy($"month" asc, $"year" asc).show()
 //df.groupBy($"""hostname""").agg(count("*") as "hits").orderBy($"hits" desc).limit(20).show()
--- scripts/get-list-of-unique-ips.scala
+++ scripts/get-list-of-unique-ips.scala
@@ -3,11 +3,14 @@ import org.apache.spark.sql.DataFrame
 import swiss.sib.analytics.server.logs.utils.LConfigUtils
 import swiss.sib.analytics.server.logs.utils.LogEntryUtils
 
+val start = System.currentTimeMillis();
+
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val config = LConfigUtils.readConfigFile(System.getProperty("config.file"));
 
-val df = sqlContext.read.parquet(config.parquetFile.getPath)
+val start = System.currentTimeMillis();
 
+val df = sqlContext.read.parquet(config.parquetFile.getPath)
 
 val fileName = readLine("Save Distinct IP file")
 
@@ -18,3 +21,5 @@ val fileName = readLine("Save Distinct IP file")
 val resultDF = df.groupBy($"""clientInfo.ipAddress""").agg(count("*") as "hits").orderBy($"hits" desc)
 
 resultDF.coalesce(1).write.option("header", "true").csv(fileName)
+
+println("Finished in " + (System.currentTimeMillis() - start) / (60 * 1000.0) + " min")
