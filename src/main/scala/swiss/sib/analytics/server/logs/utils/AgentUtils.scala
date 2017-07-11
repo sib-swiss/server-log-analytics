72e060a2836fdfec4f084394403b37a4b99d744d
Daniel Teixeira <ddtxra@gmail.com>
2017-07-11 10:37:56 +0200

    adding patterns in lowercase

--- src/main/scala/swiss/sib/analytics/server/logs/utils/AgentUtils.scala
+++ src/main/scala/swiss/sib/analytics/server/logs/utils/AgentUtils.scala
@@ -2,20 +2,20 @@ package swiss.sib.analytics.server.logs.utils
 
 object AgentUtils {
 
-  val BOT_PATTERN = """(.*)(?i)(bot|crawler|spider|pingdom|monitis|teoma|facebook|bing|yahoo|embedly|quora|outbrain|pinterest|vkshare|validator)(.*)""".r
+  val BOT_PATTERN = """(.*)(?i)(googlebot|yandexbot|baidupsider|bot|crawler|spider|pingdom|monitis|teoma|facebook|bing|yahoo|embedly|quora|outbrain|pinterest|vkshare|validator)(.*)""".r
 
   def getBotInfo(agent: String): (String, Boolean) = {
     agent match {
-      case BOT_PATTERN(_, bot, _) => return (bot, true);
+      case BOT_PATTERN(_, bot, _) => return (bot.toLowerCase().trim(), true);
       case _                      => return ("not-bot-pattern", false);
     }
   }
 
-  val PROGRAM_PATTERN = """(.*)(?i)(python|java|perl|wget|ruby|curl)(.*)""".r
+  val PROGRAM_PATTERN = """(.*)(?i)(python|java|perl|wget|ruby|curl|php)(.*)""".r
 
   def getProgramInfo(agent: String): (String, Boolean) = {
     agent match {
-      case PROGRAM_PATTERN(_, program, _) => return (program, true);
+      case PROGRAM_PATTERN(_, program, _) => return (program.toLowerCase().trim(), true);
       case _                              => return ("not-program-pattern", false);
     }
   }
