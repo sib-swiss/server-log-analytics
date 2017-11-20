6cf40cf6f9d627ad7974c2ced577ce817a410ed3
Daniel Teixeira <ddtxra@gmail.com>
2017-11-20 15:59:57 +0100

    adding bots and non bots

--- scripts/generate-insights-report.scala
+++ scripts/generate-insights-report.scala
@@ -12,7 +12,7 @@ val year = scala.io.StdIn.readLine("Year (ex. 2016) : ").toInt
 
 val start = System.currentTimeMillis();
 
-val df = sqlContext.read.parquet(config.parquetFile.getPath).filter($"year" === year)
+val dfForYear = sqlContext.read.parquet(config.parquetFile.getPath).filter($"year" === year)
 
 def createFileWriter (path : String) = {
     val file = new java.io.File(path)
@@ -30,7 +30,8 @@ def prt(s: String, fw: java.io.FileWriter = null) = {
 val maxResults = 100;
 
 val metrics = List(
-  ("server_hits", count("*") as "server_hits", 
+    
+  ("server_hits", false, count("*") as "server_hits", 
 	List(("responseInfo.contentPresent", "content_length_present"), 
             ("agentInfo.isBot", "bot_traffic"), ("agentInfo.bot", "bot"), ("agentInfo.agent", "agent"),
             ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls"),  
@@ -38,9 +39,28 @@ val metrics = List(
             ("responseInfo.charset", "charset"), ("responseInfo.status", "status_code"),
             ("clientInfo.ipAddress", "top_ips")))
 
+  , 
+  
+  ("server_hits_without_bots", true, count("*") as "server_hits", 
+	List(("responseInfo.contentPresent", "content_length_present"), 
+            ("agentInfo.agent", "agent"),
+            ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls"),  
+	          ("agentInfo.isProgram", "programmatic_access"), ("agentInfo.program", "programmatic"),
+            ("responseInfo.charset", "charset"), ("responseInfo.status", "status_code"),
+            ("clientInfo.ipAddress", "top_ips")))
+
   ,  
   
-  ("server_throughput", sum("responseInfo.contentSize") as "server_throughput",
+  ("server_throughput", false, sum("responseInfo.contentSize") as "server_throughput",
+  List(("agentInfo.agent", "agent"),
+    ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls"),	
+	    ("agentInfo.isProgram", "programmatic_access"), ("agentInfo.program", "programmatic"),
+            ("responseInfo.charset", "charset"), ("responseInfo.status", "status_code"), 
+	    ("clientInfo.ipAddress", "top_ips")))
+
+  ,
+
+  ("server_throughput_without_bots", true, sum("responseInfo.contentSize") as "server_throughput",
   List(("agentInfo.isBot", "bot_traffic"), ("agentInfo.bot", "bot"), ("agentInfo.agent", "agent"),
     ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls"),	
 	    ("agentInfo.isProgram", "programmatic_access"), ("agentInfo.program", "programmatic"),
@@ -48,21 +68,31 @@ val metrics = List(
 	    ("clientInfo.ipAddress", "top_ips")))
 
   ,
-  
-  ("server_distinct_ips", countDistinct("clientInfo.ipAddress") as "server_distinct_ips",
-	List(("agentInfo.isBot", "bot_traffic"), ("agentInfo.bot", "bot"), ("agentInfo.agent", "agent")))  
+
+  ("server_distinct_ips", false, countDistinct("clientInfo.ipAddress") as "server_distinct_ips",
+	List(("agentInfo.isBot", "bot_traffic"), ("agentInfo.bot", "bot"), ("agentInfo.agent", "agent"), ("agentInfo.program", "programmatic"),  ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls")))  ,
+	
+  ,
+
+  ("server_distinct_ips_without_bots", true, countDistinct("clientInfo.ipAddress") as "server_distinct_ips",
+	List(("agentInfo.agent", "agent"), ("agentInfo.program", "programmatic"),  ("requestInfo.firstLevelPath", "first_level_path"), ("requestInfo.url", "top_urls")))
+
 
 )
 
 metrics.foreach(m => {
   m match {
-    case (metric, function, dimensions) => {
+    case (metric, filterBot, function, dimensions) => {
 
-      val fileName = name + "/" + year + "/" + m._1 
-      val fw = createFileWriter(fileName + "/" + name + "-" + year + "-" + m._1 + ".tsv")
+      val fileName = name + "/" + year + "/" + metric 
+      val fw = createFileWriter(fileName + "/" + name + "-" + year + "-" + metric + ".tsv")
 
       prt(metric + "_per_months_ ################################# ")
 	
+      val df = if(filterBot) {
+        dfForYear.filter($"isBot" === true)
+      }else dfForYear
+        
       //Shows metrics by date
       val total : Number = df.groupBy($"year").agg(function).first().get(1).asInstanceOf[Number]
       df.groupBy($"month", $"year").agg(function).orderBy($"year" asc, $"month" asc).collect().foreach(r => prt(r.mkString("\t"), fw));
