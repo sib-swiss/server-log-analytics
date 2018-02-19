59517524aacee237796e6f16f32cf55fb9a537d9
Daniel <ddtxra@gmail.com>
2018-02-19 16:30:12 +0100

    adding sync file

---
+++ distinct-ips/sync-with-archive.sh
@@ -0,0 +1,1 @@
+rsync -avz --include "*/" --include="*.csv" --exclude="*" /home/dteixeir/server-log-analytics/distinct-ips dteixeir@dev.vital-it.ch:/archive/sib/sib_tech/projects/server-logs/2017/distinct-ips
